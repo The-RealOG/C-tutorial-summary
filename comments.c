1cd88b6f579f719f6bf6bc7ebe36d117afd7ddf7
Chibueze Anyachebelu <168302805+The-RealOG@users.noreply.github.com>
2025-05-16 00:45:22 -0500

    added calculator.c

--- comments.c
+++ comments.c
@@ -11,10 +11,16 @@ int main() {
 
 
     //getting user input in c
-    printf("please enter your grade: ");
-    int grade;
+    // printf("please enter your grade: ");
+    // int grade;
 
-    scanf("%d", &grade);
+    // scanf("%d", &grade);
+    char name[20];
+
+    //fgets function lets you get lots of characters simultaneously (including spaces)
+    printf("Enter your name: ");
+    fgets(name, 20, stdin); //stdin refers to the terminal input and also adds an \n char which prints a newline right after you call the variable
+    printf("Your name is %s\n", name);
 
     return 0;
 }
