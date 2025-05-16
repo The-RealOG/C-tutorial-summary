404d3c1b8341ab84bc738f64ff05f34cb2486d79
Chibueze Anyachebelu <168302805+The-RealOG@users.noreply.github.com>
2025-05-16 03:11:05 -0500

    added struct.c

--- calculator.c
+++ calculator.c
@@ -4,16 +4,35 @@
 int main() {
     double num1;
     double num2;
-    char op;
+    char op; // stores the user's preferred operator
 
 
     //fgets works with strings ONLY
     printf("Enter a number: \n");
-    scanf("%d", &num1);
-    printf("Enter second number: \n");
+    scanf("%lf", &num1); // lf because num1 is of type double
 
-    scanf("%d", &num2);
-    int result = num1 + num2;
-    printf("Answer: %d", result);
+    printf("Enter operator: ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: \n");
+    scanf("%lf", &num2);
+
+    //int result = num1 + num2;
+    //printf("Answer: %d", result);
+
+
+    if (op == '+') {
+        printf("%f", num1 + num2);
+    } else if (op == '-') {
+        printf("%f", num1 - num2);
+    } else if (op == '/') {
+        printf("%f", num1 / num2);
+    } else if (op == '*') {
+        printf("%f", num1 * num2);
+    } else{
+        printf("Invalid statement");
+    }
+
+    return 0;
     
 }
