3995680c162235b95403442445c14bc7b495cb4f
Chibueze Anyachebelu <168302805+The-RealOG@users.noreply.github.com>
2025-05-16 14:52:33 -0500

    added 2darrays.c

---
+++ 2darrays.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    int nums[3][2] = { //creates a 2d array
+                        {1, 2},
+                        {3, 4},
+                        {5, 6},
+    };
+
+    return 0;
+}
