924b15a6c50a238e68c3f45dd27d7d9e45989335
Chibueze Anyachebelu <168302805+The-RealOG@users.noreply.github.com>
2025-05-15 23:58:58 -0500

    basic data types

---
+++ make.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // char characterName[] = "George";
+    // int characterAge = 35;
+
+    // printf("There once was a man named %s\n", characterName);
+    // printf("he was %d years old.\n", characterAge);
+    // printf("He really liked the name %s.\n", characterName);
+    // printf("but did not like being %d.\n", characterAge);
+
+    int age = 40;
+    double gpa = 3.6;
+    char grade = 'A'; // char typically get made using the single strokes
+    char phrase[] = "I am the GOAT.";
+    return 0;
+}
